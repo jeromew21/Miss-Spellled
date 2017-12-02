c9287e668a07116208de19a5d2266122ee08a176
Jerome Wei <jeromew@berkeley.edu>
2017-12-02 02:34:54 -0800

    Works decently with some issues

--- miss_spellled.js
+++ miss_spellled.js
@@ -1,1 +1,45 @@
-alert("Fuck You")
+init = function() {
+    var scrambleInside = function(inside) {
+        if (inside.length < 2) {
+            return inside
+        }
+        a = inside.split(""),
+
+        j = Math.floor(Math.random() * (a.length + 1));
+        k = Math.floor(Math.random() * (a.length + 1));
+        tmp = a[j]
+        a[j] = a[k];
+        a[k] = tmp
+        return a.join("");
+    }
+
+    var scramble = function(word) {
+        if (word == null) {
+            return word
+        }
+        if (word.length >= 3) {
+            return word.substring(0, 1) + scrambleInside(word.substring(1, word.length-1)) + word.substring(word.length-1, word.length)
+        }
+        return word
+    }
+
+    doReplace = function(element) {
+        if (element == undefined) return
+        if (element.childNodes.length > 0)
+            for (var i = 0; i < element.childNodes.length; i++)
+                doReplace(element.childNodes[i]);
+
+        if (element.nodeType == Node.TEXT_NODE && /\S/.test(element.nodeValue))
+            element.nodeValue = scramble(element.nodeValue);
+    }
+
+    document.getElementsByTagName("html")[0].style.display = "none";
+
+    window.onload = function() {
+        b = document.getElementsByTagName("body")[0]
+        doReplace(b);
+        document.getElementsByTagName("html")[0].style.display = "block";
+    }
+}
+
+init()
