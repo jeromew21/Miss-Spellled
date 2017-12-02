a70d41673e761366e8907f81deba748caf91d6ec
Jerome Wei <jeromew@berkeley.edu>
2017-12-02 15:14:15 -0800

    fixed stuff

--- miss_spellled.js
+++ miss_spellled.js
@@ -1,4 +1,6 @@
 init = function() {
+    letters = "qwertyuiopasdfghjklzxcvbnm".split("");
+
     var scrambleInside = function(inside) {
         if (inside.length < 2) {
             return inside
@@ -7,13 +9,16 @@ init = function() {
 
         j = Math.floor(Math.random() * (a.length + 1));
         k = Math.floor(Math.random() * (a.length + 1));
-        tmp = a[j]
-        a[j] = a[k];
-        a[k] = tmp
-        return a.join("");
+        if (letters.indexOf(a[j]) != -1 && letters.indexOf(a[k]) != -1) {
+            tmp = a[j]
+            a[j] = a[k];
+            a[k] = tmp
+            return a.join("");
+        }
+        return inside
     }
 
-    var scramble = function(word) {
+    var scrambleWord = function(word) {
         if (word == null) {
             return word
         }
@@ -29,8 +34,13 @@ init = function() {
             for (var i = 0; i < element.childNodes.length; i++)
                 doReplace(element.childNodes[i]);
 
-        if (element.nodeType == Node.TEXT_NODE && /\S/.test(element.nodeValue))
-            element.nodeValue = scramble(element.nodeValue);
+        if (element.nodeType == Node.TEXT_NODE && /\S/.test(element.nodeValue)) {
+            splits = element.nodeValue.split(" ");
+            for (var i = 0; i < splits.length; i++) {
+                splits[i] = scrambleWord(splits[i]);
+            }
+            element.nodeValue = splits.join(" ");
+        }
     }
 
     document.getElementsByTagName("html")[0].style.display = "none";
